a804e6bf10c2d81a8003cf45295c1c51f315ac62
Your Name <you@example.com>
2021-12-29 13:11:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const io = require("socket.io")(8900, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+  let users = [] ;
+  const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+      users.push({ userId, socketId });
+  };
+  const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+  };
+  const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+  io.on("connection", (socket) => {
+    //when connect
+    console.log("a user connected.");
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user?.socketId).emit("getMessage", {
+          senderId,
+          text,
+        });
+      });
+    //io.emit("welcome","Welcome to socket server") ; // T send this to all the users
+    socket.on("addUser",userId=>{
+        console.log("Added an user") ;
+    addUser(userId,socket.Id) ;
+    io.emit("getUsers",users) ;
+    })
+    socket.on("disconnect",()=>{
+        console.log("a user disconnected!");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    })
+    });
